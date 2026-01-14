5b174f24367acbe2a952382331d72531cf310149
landonedwards <lando.s.edwards@gmail.com>
2026-01-13 17:32:25 -0700

    completed intro to routing

--- server.js
+++ server.js
@@ -1,14 +1,37 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+// const name = process.env.NAME; // <-- NEW
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-const name = process.env.NAME; // <-- NEW
+/**
+ * Configure Express middleware
+ */
 
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+/**
+ * Routes
+ */
 app.get('/', (req, res) => {
-    res.send(`Hello, ${name}!`); // <-- UPDATED
+    res.sendFile(path.join(__dirname, 'src/views/home.html'));
 });
 
-const PORT = 3000;
+app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/about.html'));
+});
+
+app.get('/products', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/views/products.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://127.0.0.1:${PORT}`);
 });
