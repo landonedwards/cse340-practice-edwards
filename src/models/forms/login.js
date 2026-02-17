4e9e1dae94be8251807c817bdffb07b82dd20780
landonedwards <lando.s.edwards@gmail.com>
2026-02-17 00:12:28 -0700

    completed account management activity

--- src/models/forms/login.js
+++ src/models/forms/login.js
@@ -9,14 +9,20 @@ import db from '../db.js';
  */
 const findUserByEmail = async (email) => {
     const query = `
-    SELECT id, name, email, password, created_at
-    FROM users
-    WHERE LOWER(email) = LOWER($1)
-    LIMIT 1
+        SELECT 
+            users.id, 
+            users.name, 
+            users.email, 
+            users.password, 
+            users.created_at,
+            roles.role_name AS "roleName"
+        FROM users
+        INNER JOIN roles ON users.role_id = roles.id
+        WHERE LOWER(users.email) = LOWER($1)
+        LIMIT 1
     `;
-
     const result = await db.query(query, [email]);
-    return result.rows[0];
+    return result.rows[0] || null;
 };
 
 /**
